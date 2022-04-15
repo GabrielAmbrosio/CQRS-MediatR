84fff250c8ff720fc240c96e44a40c652893cd60
GabrielAmbrosio <gabe6614@gmail.com>
2022-04-15 18:08:00 -0300

    CQRS MediatR Practise

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Gabriel", LastName = "Ambrosio" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Marcelo", LastName = "Godoy" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel personModel = new PersonModel { FirstName = firstName, LastName = lastName };
+            personModel.Id = people.Max(p => p.Id) + 1;
+            people.Add(personModel);
+            return personModel;
+        }
+    }
+}
